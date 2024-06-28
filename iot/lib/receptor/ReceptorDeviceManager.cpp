3cc5a5245e7b61b196f9fc60220be9375f982254
Clemathe <74459537+Clemathe@users.noreply.github.com>
2024-06-28 15:19:12 +0200

    MAJ - Add ID emetter auth

--- iot/lib/receptor/ReceptorDeviceManager.cpp
+++ iot/lib/receptor/ReceptorDeviceManager.cpp
@@ -61,7 +61,9 @@ void ReceptorDeviceManager::processReceivedPacket(std::vector<uint8_t> &packet) 
     int y = 0;
     for (const std::shared_ptr<measure::AbstractMeasure> &measure: dataManager.measures.measures) {
         String value = measure->getDeSerializedMeasure();
-        Heltec.display->drawString(5, y, String(value.c_str()));
+        // Affichage des valeurs sur l'écran
+        Heltec.display->drawString(2, y, String(measure->getInfos().sensorTypeToString(measure->getInfos().sensorType[0])));
+        Heltec.display->drawString(90, y, String(value.c_str()));
         y += 10; // Ajuster l'espacement en fonction de vos besoins
 
         //Création des données à envoyer sur l'API
